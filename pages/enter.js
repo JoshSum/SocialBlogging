47a3c437201ab23e789a29ad9fd7587b905913fd
rs-jsumardi <jsumardi@dspratama.co.id>
2022-04-05 16:46:11 +0700

    🚀 Initial Commit

---
+++ pages/enter.js
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+import { googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+    const {user, username} = useContext(UserContext);
+
+    // 1. user signed out <SignInButton />
+    // 2. user signed in, but missing username <UsernameForm />
+    // 3. user signed in, and has username <SignOutButton />
+    return(
+        <main>
+            {user ? 
+            !username ? <UsernameForm /> : <SignOutButton/> 
+            : <SignInButton/>
+            }
+        </main>
+    );
+}
+
+// Sign in with Google button
+function SignInButton() {
+ const signInWithGoogle = async () => {
+     await auth.signInWithPopup(googleAuthProvider);
+ }
+ return (
+        <button className='btn-google' onClick={signInWithGoogle}>
+            <img src={'./google.png'} alt='Sign in with Google' />Sign in with Google
+        </button>
+ )  
+}
+
+//Sign out button
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign out</button>;
+}
+
+function UsernameForm() {
+
+}
